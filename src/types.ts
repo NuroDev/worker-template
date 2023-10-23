c4717b94f1a88736f659c9fa43f5e389138015c4
nurodev <me@nuro.dev>
2023-10-23 19:17:39 +0100

    Updated placeholder types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,25 @@
-type Bindings = {};
+import type { DurableObjectNamespace, Fetcher, KVNamespace, Queue, R2Bucket } from '@cloudflare/workers-types';
+
+type Bindings = {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+	//
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
+};
 
 type Variables = {};
 
 export interface Env {
-  Bindings: Bindings;
-  Variables: Variables;
+	Bindings: Bindings;
+	Variables: Variables;
 }
